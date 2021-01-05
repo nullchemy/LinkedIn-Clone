71f552d1bef08e19c612641782ec9eee01e53031
DennisRono <finnneron1@gmail.com>
2021-01-05 11:43:02 +0300

    wooow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Body from './components/Body.jsx';
 import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className="body">
+            <div className="leftb">
+                <div className="top">
+                    
+                </div>
+                <div className="bottom">
+
+                </div>
+            </div>
+            <div className="middle">
+                <div className="post">
+                    <div className="form-post">
+                        <input type="text" className="input-post" placeholder="Type your post"/>
+                    </div>
+                </div>
+                <div className="content">
+
+                </div>
+            </div>
+            <div className="rightb">
+                <div className="ads">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body
