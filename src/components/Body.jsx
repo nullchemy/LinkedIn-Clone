fc102fff7cc46d7ef3aa5e9a28ea3d0d00fcbe62
DennisRono <finnneron1@gmail.com>
2021-01-06 12:41:17 +0300

    New Commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -33,7 +33,12 @@ function Body() {
                     </div>
                 </div>
                 <div className="bottom">
-
+                    <div className="title">
+                    <p>Recent</p>
+                    <p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
+                    <path d="M15 11L8 6.39 1 11V8.61L8 4l7 4.61z"></path>
+                    </svg></p>
+                    </div>
                 </div>
             </div>
             <div className="middle">
