3ceb3e8f686cb7bcff9fd2d274192328642991f5
unknown <finnneron1@gmail.com>
2022-03-05 21:35:02 +0300

    compound funny pleasant sat ship

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../styles/css/navbar.css'
+import NavLogo as ReactComponent from '../assets/svg/navlogo.svg'
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <div className="navcontainer">
-            
+            <div className="navflex">
+                <div className="navleft">
+                    <div className="navlogo">
+                        <NavLogo />
+                    </div>
+                </div>
+                <div className="navright"></div>
+            </div>
         </div>
     </div>
   )
