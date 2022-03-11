392a205bcc53d6cbb8e43d4ac85f5d1449190ce9
DennisRono <finnneron1@gmail.com>
2022-03-11 22:03:01 +0300

    as border frozen alphabet composition

--- client/src/routes/Network.jsx
+++ client/src/routes/Network.jsx
@@ -9,7 +9,6 @@ const Network = () => {
   const [clicked, setClicked] = useState(false);
   const containerRef = useRef(null);
   const linksRef = useRef(null);
-  let useridKey = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
 
   useEffect(() => {
     const linksHeight = linksRef.current.getBoundingClientRect().height;
