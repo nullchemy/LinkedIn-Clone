4d11f805ceb97a82d0c378a4fd3d4acec963b9e9
DennisRono <finnneron1@gmail.com>
2022-03-12 18:51:40 +0300

    limited stairs sit pencil machinery

--- client/src/routes/Notifications.jsx
+++ client/src/routes/Notifications.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../styles/css/notifications.css'
 
 const Notifications = () => {
   return (
-    <div>Notifications</div>
+    <div className="notificationsPage">
+      <div className="notifwrapper">
+        <div className="notifcontainer">
+          <div className="notifFlex">
+            <div className="notifLeft"></div>
+            <div className="notifMiddle"></div>
+            <div className="notifRight"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
