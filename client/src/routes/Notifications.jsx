1e20d530b40daee072d6c67491e96613041f5c86
DennisRono <finnneron1@gmail.com>
2022-03-12 19:52:01 +0300

    powder nest cast dollar doing

--- client/src/routes/Notifications.jsx
+++ client/src/routes/Notifications.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/css/notifications.css'
 import { Link } from 'react-router-dom'
+import { notifications } from "../data/notifications";
 
 const Notifications = () => {
   return (
