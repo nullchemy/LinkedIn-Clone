0d4a786e722f58e829ce94b0c36b851f80b34be3
DennisRono <finnneron1@gmail.com>
2022-03-12 09:24:01 +0300

    judge short deep wood settle

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,6 +26,24 @@ const Navbar = () => {
             document.querySelector(".nav-dropdown").classList.remove("show");
         }
     }, [width]);
+    function useOutsideAlerter(ref) {
+        useEffect(() => {
+            /**
+             * Alert if clicked on outside of element
+             */
+            function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                alert("You clicked outside of me!");
+            }
+            }
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
     const navDrop = () => {
         if(width < 920){
             document.querySelector(".nav-dropdown").classList.toggle("show");
