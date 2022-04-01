4a560c6dd070ea3c8c3f4ecf719377df1d3c8612
DennisRono <finnneron1@gmail.com>
2022-04-01 16:53:05 +0300

    smooth missing else knowledge phrase

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/css/home.css'
+import Footer from '../components/HomeFooter'
 
 const Home = () => {
   return (
     <div>
-
+        
     </div>
   )
 }
