6bf78ababc73a94e5e19d7a5e890c67ec504f9f2
DennisRono <finnneron1@gmail.com>
2022-04-01 19:18:32 +0300

    logo landing added

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -2,42 +2,50 @@ import React from 'react'
 import '../styles/css/home.css'
 import { Link } from 'react-router-dom'
 import Footer from '../components/HomeFooter'
-import {ReactComponent as LandingLogo} from '../assets/svg/footer-logo.svg'
+import {ReactComponent as LandingLogo} from '../assets/svg/Landing-Home-Logo.svg'
+import {ReactComponent as Discover} from '../assets/svg/discover.svg'
+import {ReactComponent as People} from '../assets/svg/people.svg'
+import {ReactComponent as Learning} from '../assets/svg/Learning.svg'
+import {ReactComponent as Jobs} from '../assets/svg/jobs.svg'
 
 const Home = () => {
   return (
-    <div>
+    <div className="landingPage">
         <header className="landingHeader">
           <div className="landingContainer">
             <div className="landingLogo">
-              <LandingLogo />
+              <LandingLogo className="LandingLogoIcon"/>
             </div>
             <div className="landingNavigation">
               <div className="lannavFlex">
-                <Link to="">
+                <Link to="" className="lannavLink">
                   <div className="lannavItem">
+                    <Discover className="lannavIcon"/>
                     <span>Discover</span>
                   </div>
                 </Link>
-                <Link to="">
+                <Link to="" className="lannavLink">
                   <div className="lannavItem">
+                    <People className="lannavIcon"/>
                     <span>People</span>
                   </div>
                 </Link>
-                <Link to="">
+                <Link to="" className="lannavLink">
                   <div className="lannavItem">
+                    <Learning className="lannavIcon"/>
                     <span>Learning</span>
                   </div>
                 </Link>
-                <Link to="">
+                <Link to="" className="lannavLink">
                   <div className="lannavItem">
+                    <Jobs className="lannavIcon"/>
                     <span>Jobs</span>
                   </div>
                 </Link>
-                <Link to="">
+                <Link to="" className="lannavLink">
                   Join now
                 </Link>
-                <Link to="">
+                <Link to="" className="lannavLink">
                   Sign in
                 </Link>
               </div>
