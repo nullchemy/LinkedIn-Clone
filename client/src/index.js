23f4fac6741ed5ba9bed72333df7f97add1e827c
DennisRono <finnneron1@gmail.com>
2022-04-01 17:20:05 +0300

    funny fresh brush ten still

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/globals.css'
 import App from './App';
+import {BrowserRouter as Router } from 'react-router-dom'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
