c9fe45f3940dfed3d8d46ca2b3d4f2787f86ba69
DennisRono <finnneron1@gmail.com>
2022-04-01 16:46:11 +0300

    joy gray third brought hurried

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import Home from "./routes/Feed";
+import Feed from "./routes/Feed";
+import Home from "./routes/Home";
 import Jobs from "./routes/Jobs";
 import Messaging from "./routes/Messaging";
 import Network from "./routes/Network";
@@ -39,9 +40,10 @@ function App() {
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/feed" element={<Home />}></Route>
+            <Route path="/feed" element={<Feed />}></Route>
             <Route path="/" element={<Navigate to="/feed" />}></Route>
             <Route path="/jobs" element={<Jobs />}></Route>
+            <Route path="/home" element={<Home />}></Route>
             <Route path="/messaging" element={<Messaging />}></Route>
             <Route path="/network" element={<Network />}></Route>
             <Route path="/notifications" element={<Notifications />}></Route>
