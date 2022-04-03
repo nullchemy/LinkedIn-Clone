29ee77ab9754fa4bcb06e20c479e3a9ef5c3d51d
DennisRono <finnneron1@gmail.com>
2022-04-03 12:06:02 +0300

    plenty began sad writing require

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './styles/css/globals.css'
 import App from './App';
 import {BrowserRouter as Router } from 'react-router-dom'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-serviceWorkerRegistration.register();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Router><App tab="home" /></Router>);
+
+serviceWorkerRegistration.unregister();
